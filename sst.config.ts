6bdd87461c0229eaac82f01417825e2af58f550e
BenBowers <37522623+BenBowers@users.noreply.github.com>
2023-12-14 16:48:16 +0800

    Add defaults for lambda functions

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,9 @@
+import { aws_lambda as lambda } from 'aws-cdk-lib';
 import { SSTConfig } from 'sst';
 import { API } from './infra/MyStack';
+
+const LAMBDA_INSIGHTS_ARN =
+  'arn:aws:lambda:ap-southeast-2:580247275435:layer:LambdaInsightsExtension-Arm64:5';
 
 export default {
   config(_input) {
@@ -8,7 +12,23 @@ export default {
       region: 'ap-southeast-2',
     };
   },
+
   stacks(app) {
+    app.setDefaultFunctionProps({
+      runtime: 'nodejs18.x',
+      insightsVersion:
+        lambda.LambdaInsightsVersion.fromInsightVersionArn(LAMBDA_INSIGHTS_ARN),
+      logRetention: 'one_month',
+      tracing: 'active',
+      architecture: 'arm_64',
+      nodejs: {
+        esbuild: {
+          keepNames: false,
+          minify: true,
+        },
+      },
+    });
+
     app.stack(API);
   },
 } satisfies SSTConfig;
