be9de927a6d637e807ca784b88513f5c66eb1c1f
Rami Ruhayel <rami@ruhayel.com>
2024-01-19 16:48:51 +0800

    Handle serverside error in job status dynamo secondary adapter

--- src/adaptors/secondary/job-status-dynamo.ts
+++ src/adaptors/secondary/job-status-dynamo.ts
@@ -1,37 +1,10 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
-import { Entity, Table } from 'dynamodb-toolbox';
-import { Config } from 'sst/node/config';
 import MealNotFoundError from '../../errors/MealNotFoundError';
 import ServerError from '../../errors/ServerError';
 
-const tableName = Config.TABLE_NAME;
-
-const DocumentClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
-
-const mealTable = new Table({
-  name: tableName,
-  partitionKey: 'mealId',
-
-  DocumentClient,
-});
-
-const meal = new Entity({
-  name: 'Meal',
-  attributes: {
-    mealId: { type: 'string', partitionKey: true },
-    jobId: { type: 'string' },
-    imageLocation: { type: 'string' },
-    jobStatus: { type: 'string' },
-    mealPrompt: { type: 'string' },
-    mealParameters: { type: 'string' },
-    mealType: { type: 'string' },
-  },
-  table: mealTable,
-});
+import { Meal } from '../../database/entities';
 
 const getJobStatus = async (mealId: string): Promise<string> => {
-  const mealItem = (await meal.get({ mealId })).Item;
+  const mealItem = (await Meal.get({ mealId })).Item;
   if (!mealItem) throw new MealNotFoundError();
   if (!mealItem.jobStatus) throw new ServerError('job status not found');
   return mealItem.jobStatus;
---
+++ src/database/entities.ts
@@ -0,0 +1,29 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { Entity, Table } from 'dynamodb-toolbox';
+import { Config } from 'sst/node/config';
+
+const tableName = Config.TABLE_NAME;
+
+const DocumentClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+
+const mealTable = new Table({
+  name: tableName,
+  partitionKey: 'mealId',
+
+  DocumentClient,
+});
+
+export const Meal = new Entity({
+  name: 'Meal',
+  attributes: {
+    mealId: { type: 'string', partitionKey: true },
+    jobId: { type: 'string' },
+    imageLocation: { type: 'string' },
+    jobStatus: { type: 'string' },
+    mealPrompt: { type: 'string' },
+    mealParameters: { type: 'string' },
+    mealType: { type: 'string' },
+  },
+  table: mealTable,
+});
