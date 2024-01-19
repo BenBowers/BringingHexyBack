492588eee3b48c9d54867760b3fb4d003f873303
Rami Ruhayel <rami@ruhayel.com>
2024-01-19 17:32:01 +0800

    A unit test for job status dynamo secondary adapter when meal entity rejects with a value that is not an error

--- src/adaptors/secondary/job-status-dynamo.ts
+++ src/adaptors/secondary/job-status-dynamo.ts
@@ -4,7 +4,15 @@ import ServerError from '../../errors/ServerError';
 import { Meal } from '../../database/entities';
 
 const getJobStatus = async (mealId: string): Promise<string> => {
-  const mealItem = (await Meal.get({ mealId })).Item;
+  let mealItem;
+  try {
+    mealItem = (await Meal.get({ mealId })).Item;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new ServerError(error.name + ': ' + error.message);
+    }
+    throw new ServerError('UnknownError');
+  }
   if (!mealItem) throw new MealNotFoundError();
   if (!mealItem.jobStatus) throw new ServerError('job status not found');
   return mealItem.jobStatus;
