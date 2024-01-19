8a913fcff9f1d61a83f5132e1d6aaaf6a79f692e
Rami Ruhayel <rami@ruhayel.com>
2024-01-19 17:32:01 +0800

    A unit test for job status dynamo secondary adapter when meal entity rejects with a value that is not an error

--- src/adaptors/secondary/job-status-dynamo.spec.ts
+++ src/adaptors/secondary/job-status-dynamo.spec.ts
@@ -14,7 +14,7 @@ describe('job-status-dynamo', () => {
   });
 
   describe('Given a meal identifier', () => {
-    describe('when the Meal entity rejects', () => {
+    describe('when the Meal entity rejects with an Error', () => {
       it('rejects with a ServerError with a message containing the error name nad the error message', async () => {
         mealGetSpy.mockRejectedValue(
           new LimitExceededException({
@@ -27,8 +27,21 @@ describe('job-status-dynamo', () => {
           (error) => {
             expect(error).toBeInstanceOf(ServerError);
             expect(error.message).toEqual(
-              'LimiteExceedException: Ben says to back off'
+              'LimitExceededException: Ben says to back off'
             );
+          }
+        );
+        expect.assertions(2);
+      });
+    });
+    describe('when the Meal entity rejects with a value that is not an Error', () => {
+      it('should throw a ServerError with the message "Unknown error"', async () => {
+        mealGetSpy.mockRejectedValue('Failed badly');
+
+        await getJobStatus('43cde2d4-b9b6-400c-847a-66d314baf588').catch(
+          (error) => {
+            expect(error).toBeInstanceOf(ServerError);
+            expect(error.message).toEqual('UnknownError');
           }
         );
         expect.assertions(2);
