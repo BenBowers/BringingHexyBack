f34d0cb2b7ce776be264b353467323d50c336024
BenBowers <37522623+BenBowers@users.noreply.github.com>
2024-02-01 17:13:08 +0800

    Add paths

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,23 @@
+/// <reference types="vitest" />
+import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
-
 export default defineConfig({
   test: {
     globals: true,
     include: ['src/**/*.spec.ts', 'infra/custom-resources/**/*.spec.ts'],
+    testTimeout: 30000,
+  },
+  logLevel: 'info',
+  esbuild: {
+    sourcemap: 'both',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@infra': resolve(__dirname, 'infra'),
+      '@adaptors': resolve(__dirname, 'src/adaptors'),
+      '@use-cases': resolve(__dirname, 'src/use-cases'),
+      '@tests': resolve(__dirname, 'tests'),
+    },
   },
 });
