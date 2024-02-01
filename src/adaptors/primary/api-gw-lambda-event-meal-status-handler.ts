b0fcd57bbe951187ca3e68a92b612474d04a9d05
Lachlan Walling <lachlan.walling@journeyone.com.au>
2024-02-01 17:31:23 +0800

    Get meal status handler invokes the use case

--- src/adaptors/primary/api-gw-lambda-event-meal-status-handler.ts
+++ src/adaptors/primary/api-gw-lambda-event-meal-status-handler.ts
@@ -1,6 +1,11 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { getMealStatus } from '@use-cases/get-meal-status';
+import { APIGatewayProxyEvent, APIGatewayProxyHandler } from 'aws-lambda';
 
-export const handler: APIGatewayProxyHandler = async (event, context) => {
+export const getMealStatusHandler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent,
+  context
+) => {
+  getMealStatus(event?.queryStringParameters?.mealId as string);
   return {
     statusCode: 400,
     body: JSON.stringify({
